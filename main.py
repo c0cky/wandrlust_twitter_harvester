82875c65a6983936b2a19778a92b2ae3bf1140d3
Camron Godbout <camron248@gmail.com>
2016-02-02 19:01:26 -0500

    working

--- main.py
+++ main.py
@@ -6,22 +6,24 @@ array = []
 
 with open("keys.txt", "r") as ins:
     for line in ins:
-        array.append(line)
-
+        array.append(line.rstrip('\n'))
+print array
 api = twitter.Api(consumer_key=array[0],
                       consumer_secret=array[1],
                       access_token_key=array[2],
                       access_token_secret=array[3])
 since_id = 0
-wait_time = api.GetAverageSleepTime()
-while (True) {
+wait_time = api.GetAverageSleepTime("/search/tweets")
+while (True):
     time.sleep(wait_time)
     results = api.GetSearch(term="travel", since_id=since_id)
     for status in results:
         time.sleep(wait_time)
         print "liking", status
-        api.CreateFavorite(status=status)
+        try:
+            api.CreateFavorite(status=status)
+        except:
+            print "problem liking", status
         since_id = status.id
         with open("statuses.txt", "a") as myfile:
-            myfile.write(status)
-}
+            myfile.write(str(status))
