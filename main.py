49f982e858c8f843b621f2eea66623859735d2df
Camron Godbout <camron248@gmail.com>
2016-05-25 14:07:36 -0400

    optimize!

--- main.py
+++ main.py
@@ -14,25 +14,37 @@ def harvest(query, out_file):
     auth = tweepy.OAuthHandler(array[0], array[1])
     auth.set_access_token(array[2], array[3])
     api = tweepy.API(auth, wait_on_rate_limit=True)
-    print array
-    api = twitter.Api(consumer_key=array[0],
-                      consumer_secret=array[1],
-                      access_token_key=array[2],
-                      access_token_secret=array[3],
-                      sleep_on_rate_limit=True)
-    since_id = 0
-    while (True):
-        results = api.GetSearch(term=query, since_id=since_id, count=100, result_type="recent")
-        for status in results:
-            since_id = status.id
-            with open(out_file, "a") as myfile:
-                myfile.write(str(status) + '\n')
-    # max_tweets = 50000
-    # searched_tweets = [status for status in tweepy.Cursor(api.search, q=query).items(max_tweets)]
-    # print len(searched_tweets)
-    # for status in searched_tweets:
-    #     with open(out_file, "a") as myfile:
-    #         myfile.write(str(status) + '\n')
+    # print array
+    # api = twitter.Api(consumer_key=array[0],
+    #                   consumer_secret=array[1],
+    #                   access_token_key=array[2],
+    #                   access_token_secret=array[3],
+    #                   sleep_on_rate_limit=True)
+    # since_id = 0
+    # while (True):
+    #     results = api.GetSearch(term=query, since_id=since_id, count=100, result_type="recent")
+    #     for status in results:
+    #         since_id = status.id
+    #         with open(out_file, "a") as myfile:
+    #             myfile.write(str(status) + '\n')
+    searched_tweets = []
+    last_id = -1
+    while len(searched_tweets) < max_tweets:
+        count = max_tweets - len(searched_tweets)
+        try:
+            new_tweets = api.search(q=query, count=count, max_id=str(last_id - 1))
+            if not new_tweets:
+                break
+            searched_tweets.extend(new_tweets)
+            print "adding: ", str(len(new_tweets))
+            last_id = new_tweets[-1].id
+        except tweepy.TweepError as e:
+        # depending on TweepError.code, one may want to retry or wait
+        # to keep things simple, we will give up on an error
+            break
+    for status in searched_tweets:
+        with open(out_file, "a") as myfile:
+            myfile.write(str(status) + '\n')
 
 def create_friends(query):
     array = []
